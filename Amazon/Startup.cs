69ee791b17c73553469f72e81f7069e89ecb5fc6
david <david@LAPTOP-7R8DTB6J>
2023-10-27 17:56:30 -0700

    Agregar archivos de proyecto.

---
+++ Amazon/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Amazon.Startup))]
+namespace Amazon
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
